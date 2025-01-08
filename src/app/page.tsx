d2ceaf5dadd2f32731238c95d575c2bec8fa6558
pascal Zhang <panamepoul@gmail.com>
2025-01-08 16:35:30 +0100

    add threejs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,60 @@
-import Image from "next/image";
+'use client';
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
+import { useControls } from 'leva'
+
+const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
+  ssr: false,
+  loading: () => (
+    <div className='flex h-96 w-full flex-col items-center justify-center'>
+      <svg className='-ml-1 mr-3 h-5 w-5 animate-spin text-black' fill='none' viewBox='0 0 24 24'>
+        <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
+        <path
+          className='opacity-75'
+          fill='currentColor'
+          d='M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+        />
+      </svg>
+    </div>
+  ),
+})
+const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
+
+const Floor = (props: JSX.IntrinsicElements['mesh']) => {
+  return (
+    <mesh
+      {...props}
+      >
+      <boxGeometry args={[10, 1, 10]}/>
+      <meshStandardMaterial color="limegreen"/>
+    </mesh>
+  )
+}
+
+function Box(props: JSX.IntrinsicElements['mesh']) {
+  return (
+    <mesh
+      {...props}
+      >
+      <boxGeometry />
+      <meshStandardMaterial color="red"/>
+    </mesh>
+  )
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { position, rotation } = useControls({ position: {value: [0, 0, 0], step: 0.1}, rotation: {value: [0, 0, 0], step: 0.1} })
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div className="w-screen h-screen">
+      {/* @ts-expect-error don't know */}
+      <View className='flex h-96 w-full flex-col items-center justify-center' orbit={true}>
+        <Suspense fallback={null} >
+          <Common color="green" />
+        <Box position={position} rotation={rotation}/>
+        <Floor position={[0, -1, 0]} rotation={[0, 0, 0]}/>
+        </Suspense>
+        </View>
     </div>
   );
 }
---
+++ src/components/canvas/View.jsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { forwardRef, Suspense, useImperativeHandle, useRef } from 'react'
+import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
+import { Three } from '@/components/helpers/components/Three'
+import { Perf } from 'r3f-perf'
+
+export const Common = ({ color }) => (
+  <Suspense fallback={null}>
+    <Perf position="top-left" />
+    {color && <color attach='background' args={[color]} />}
+    <ambientLight />
+    {/* <axesHelper args={[10]} /> */}
+    <pointLight position={[20, 30, 10]} intensity={3} decay={0.2} />
+    <pointLight position={[-10, -10, -10]} color='blue' decay={0.2} />
+    <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
+  </Suspense>
+)
+
+const View = forwardRef(({ children, orbit, ...props }, ref) => {
+  const localRef = useRef(null)
+  useImperativeHandle(ref, () => localRef.current)
+
+  return (
+    <>
+      <div ref={localRef} {...props} />
+      <Three>
+        <ViewImpl track={localRef}>
+          {children}
+          {orbit && <OrbitControls />}
+        </ViewImpl>
+      </Three>
+    </>
+  )
+})
+View.displayName = 'View'
+
+export { View }
